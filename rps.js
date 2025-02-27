ba9df9b1e42c059211ac9e9194b0a086198c97d4
csnewbie003 <195610775+csnewbie003@users.noreply.github.com>
2025-02-27 11:58:51 -0600

    Establish score variables, place human choice first with alert if invalid response, establish round play

--- rps.js
+++ rps.js
@@ -1,4 +1,21 @@
-// Get random integer 
+// Get prompt from Human and output Human Choice
+
+let getHumanChoice = prompt("Choose 0 for Rock, 1 for Paper, 2 for Scissors");
+    if (getHumanChoice == 0) {
+        console.log("Human chooses Rock!");
+    }
+    else if (getHumanChoice == 1) {
+        console.log("Human chooses Rock!");
+    }
+    else if (getHumanChoice == 2) {
+        console.log("Human chooses Scissors!");
+    }
+    else {
+        alert("Invalid Response!");
+    }
+    
+
+// Get random integer and output Computer Choice
 
 let getComputerChoice = Math.floor(Math.random() * 3);
 
@@ -12,16 +29,71 @@ let getComputerChoice = Math.floor(Math.random() * 3);
         console.log("Computer chooses Scissors!");
     }
 
-let getHumanChoice = prompt("Choose 0 for Rock, 1 for Paper, 2 for Scissors");
-    if (getHumanChoice === 0) {
-        console.log("Human chooses Rock!");
-    }
-    else if (getHumanChoice === 1) {
-        console.log("Human chooses Rock!");
-    }
-    else {
-        console.log("Human chooses Scissors!");
-    }
+// Establish Score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+//Play Round
+
+if (getHumanChoice === 0 && getComputerChoice === 0) {
+    humanScore;
+    computerScore;
+    console.log("Tie!");
+}
+else if (getHumanChoice === 0 && getComputerChoice === 1) {
+    humanScore;
+    computerScore += 1;
+    console.log("Computer Wins!");
+}
+else if (getHumanChoice === 0 && getComputerChoice === 2) {
+    humanScore += 1;
+    computerScore;
+    console.log("Human Wins!");
+}
+
+if (getHumanChoice === 1 && getComputerChoice === 0) {
+    humanScore += 1;
+    computerScore;
+    console.log("Human Wins!");
+}
+else if (getHumanChoice === 1 && getComputerChoice === 1) {
+    humanScore;
+    computerScore;
+    console.log("Tie!");
+}
+else if (getHumanChoice === 1 && getComputerChoice === 2) {
+    humanScore;
+    computerScore += 1;
+    console.log("Computer Wins!");
+}
+
+if (getHumanChoice === 2 && getComputerChoice === 0) {
+    humanScore;
+    computerScore += 1;
+    console.log("Computer Wins!");
+}
+else if (getHumanChoice ===2 && getComputerChoice === 1) {
+    humanScore += 1;
+    computerScore;
+    console.log("Human Wins!");
+}
+else if (getHumanChoice === 2 && getComputerChoice === 2) {
+    humanScore;
+    computerScore;
+    console.log("Tie!");
+}
+
+
+
+console.log("Human score " + humanScore, "Computer score " + computerScore);
+
+
+
+
+
+
+
 
 
     
