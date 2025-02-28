eb3457e58a1330908370432977e35883f902743d
csnewbie003 <195610775+csnewbie003@users.noreply.github.com>
2025-02-28 10:47:46 -0600

    Complete playround framework

--- rps.js
+++ rps.js
@@ -30,6 +30,42 @@ let computerScore = 0;
 
 // Define Round
 
-function playRound() {
-    
+function playRound(human, computer) {
+    if (human === "rock" && computer === "rock") {
+        return alert("Tie!");
+    }
+    else if (human === "rock" && computer === "paper") {
+        computerScore++;
+        return alert("Paper beats Rock.  Computer wins this round!");
+    }
+    else if (human === "rock" && computer === "scissors") {
+        humanScore++;
+        return alert("Rock beats Scissors.  Human wins this round!")
+    }
+    else if (human === "paper" && computer === "rock") {
+        humanScore++;
+        return alert("Paper beats Rock.  Human wins this round!");
+    }
+    else if (human === "paper" && computer === "paper") {
+        return alert("Tie!");
+    }
+    else if (human === "paper" && computer === "scissors") {
+        computerScore++;
+        return alert("Scissors beats Paper.  Computer wins this round!")
+    }
+    else if (human === "scissors" && computer === "rock") {
+        computerScore++;
+        return alert("Rock beats Scissors.  Computer wins this round!");
+    }
+    else if (human === "scissors" && computer === "paper") {
+        humanScore++;
+        return alert("Scissors beats Paper.  Human wins this round!");
+    }
+    else if (human === "scissors" && computer === "scissors") {
+        return alert("Tie!");
+    }
+
+
+
+
 }
