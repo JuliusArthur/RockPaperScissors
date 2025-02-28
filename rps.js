c0cfdb9471ab513c90a6fbac9fa59336f10dd63b
csnewbie003 <195610775+csnewbie003@users.noreply.github.com>
2025-02-28 09:53:01 -0600

    Wrap computer choice and human choice into playRound function

--- rps.js
+++ rps.js
@@ -1,46 +1,34 @@
-function getComputerChoice() {
-    getComputerChoice = Math.floor(Math.random() * 3);
-    if (getComputerChoice) {
-        if (getComputerChoice == 0) {
-            console.log("Computer chooses Rock");
-        }
-        else if (getComputerChoice == 1) {
-            console.log("Computer chooses Paper");
-        }
-        else if (getComputerChoice == 2) {
-            console.log("Computer chooses Scissors")
-        }
-    }
-}
-
-// Code Human Choice
-
-function getHumanChoice() {
-    getHumanChoice = prompt("Choose Rock, Paper, or Scissors! Spell correctly!");
-    getHumanChoice = getHumanChoice.toLowerCase();
-            if (getHumanChoice == "rock") {
-                console.log("Human chooses Rock");
-            }
-            else if (getHumanChoice == "paper") {
-                console.log("Human chooses Paper");
-            }
-            else if (getHumanChoice == "scissors") {
-                console.log("Human chooses Scissors");
-            }
-            else {
-                alert("Invalid Response!");
-            }
-}
-
-// Declare Score and Move Variables
+// Play Single Round
 
 humanScore = 0;
 computerScore = 0;
 
-// Play Single Round
-
 function playRound() {
-    
+    let getComputerChoice = Math.floor(Math.random() * 3);
+        let computerChoice; {
+            if (getComputerChoice === 0) {
+                computerChoice = "rock";
+            }
+            else if (getComputerChoice === 1) {
+                computerChoice = "paper";
+            }
+            else if (getComputerChoice === 2) {
+                computerChoice = "scissors";
+            }
+
+    let getHumanChoice = prompt("Choose Rock, Paper, or Scissors! Spell correctly!");
+        let humanChoice = getHumanChoice.toLowerCase();
+            if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+                return humanChoice;
+            }
+            else {
+                alert("Invalid Response! Try Again.");
+            }
+            
+   
+    }    
 }
+
+playRound();
 
 // Play Entire Game
