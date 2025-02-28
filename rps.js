7a5a00a8fa30b323c6102d1f0a47f9bfef728ad8
csnewbie003 <195610775+csnewbie003@users.noreply.github.com>
2025-02-28 11:13:40 -0600

    Finalize framework for gameplay

--- rps.js
+++ rps.js
@@ -64,8 +64,33 @@ function playRound(human, computer) {
     else if (human === "scissors" && computer === "scissors") {
         return alert("Tie!");
     }
+}
 
+// Define Game
 
+function playGame() {
+    alert("Welcome to Rock, Paper, Scissors. We're going to play 5 rounds!");
+    playRound(getHumanChoice(), getComputerChoice());
 
+    alert("After first round, the score is: You: ${humanScore}. Computer: ${computerScore}. Next Round!");
+    playRound(getHumanChoice(), getComputerChoice());
 
+    alert("After second round, the score is: You: ${humanScore}.  Computer: ${computerScore}. Next Round!");
+    playRound(getHumanChoice(), getComputerChoice());
+
+    alert("After third Round, the score is: You: ${humanScore}.  Computer: ${computerScore}.  Next Round!");
+    playRound(getHumanChoice(), getComputerChoice());
+
+    alert("After four rounds, the score is: You: ${humanScore}.  Computer: ${computerScore}. Final Round!");
+    playRound(getHumanChoice(), getComputerChoice());
+
+    if (humanScore > computerScore) {
+        alert("Congratulations, you win the game!");
+    }
+    else if (humanScore < computerScore) {
+        alert("I'm sorry!  You lose the game!");
+    }
+    else if (humanScore === computerScore) {
+        alert("You tied the computer!");
+    }
 }
